d473dd22b278302a0839cab8011532fc07eaa19e
rbcunha79 <rbcunha@yahoo.com>
2023-08-18 10:20:38 -0300

    -

--- proto/create-artifacts.bat
+++ proto/create-artifacts.bat
@@ -1,5 +1,15 @@
 @echo off
 
-call protoc --java_out=./src/main/java common.proto
+call protoc --java_out=../java/src/main/java common.proto
 
-call protoc --java_out=./src/main/java bank_transactions.proto
+if not %errorlevel% == 0 goto end
+
+call protoc --java_out=../java/src/main/java bank_transactions.proto
+
+if not %errorlevel% == 0 goto end
+
+echo --------------------------------
+echo Successfully created artifacts
+echo --------------------------------
+
+:end
